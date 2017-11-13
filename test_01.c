00a53449fa5e803ebe2a924e758f48cda45dd026
Lavinia Medeiros <laviniameds@icloud.com>
2017-11-13 20:07:45 -0300

    ali_find testada; funcionando

--- test_01.c
+++ test_01.c
@@ -5,7 +5,7 @@
 int main(){
   array_list_int a1 = ali_create();
   int i,t;
-  for (i=0; i < 10; ++i)
+  for (i=0; i < 8; ++i)
     ali_push_back(a1,i+1);
   t = ali_size(a1);
   printf("Tamanho de a1: %d\n",t);
@@ -13,14 +13,7 @@ int main(){
   for (i=0; i < t ; ++i)
     printf("%d ", ali_get(a1,i));
   printf("}\n");
-  /*for (i=0; i < 8 ; ++i)
-    ali_pop_back(a1);
-  t = ali_size(a1);
-  printf("Tamanho de a1: %d\n",t);
-  printf("a1 = { ");
-  for (i=0; i < t ; ++i)
-    printf("%d ", ali_get(a1,i));
-  printf("}\n");*/
+  printf("O numero %d está no indice %d\n", 1, ali_find(a1, 1));
   ali_destroy(a1);
   return 0;
 }
