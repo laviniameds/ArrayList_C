6c3e9c307c18891153f63697e885e14f656983fc
Lavinia Medeiros <laviniameds@icloud.com>
2017-11-13 20:27:28 -0300

    ali_percent_ocupped testado; funcionando

--- test_01.c
+++ test_01.c
@@ -12,17 +12,20 @@ int main(){
   for (i=0; i < ali_size(a1) ; ++i)
     printf("%d ", ali_get(a1,i));
   printf("}\n");
+  printf("porcento ocupado: %lf\n" ,ali_percent_occuped(a1));
   printf("O numero %d está no indice %d\n", 1, ali_find(a1, 1));
   ali_insert_at(a1, 5, 21);
   printf("a1 = { ");
   for (i=0; i < ali_size(a1) ; ++i)
     printf("%d ", ali_get(a1,i));
   printf("}\n");
+  printf("porcento ocupado: %lf\n" ,ali_percent_occuped(a1));
   ali_remove_from(a1, 5);
   printf("a1 = { ");
   for (i=0; i < ali_size(a1) ; ++i)
     printf("%d ", ali_get(a1,i));
   printf("}\n");
+  printf("porcento ocupado: %lf\n" ,ali_percent_occuped(a1));
 
   ali_destroy(a1);
   return 0;
