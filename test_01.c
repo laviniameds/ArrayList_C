a6512cde559f49b491a21090916a0be3a4b688c6
Lavinia Medeiros <laviniameds@icloud.com>
2017-11-13 20:16:46 -0300

    ali_insert_at testado. Funcionando

--- test_01.c
+++ test_01.c
@@ -14,6 +14,12 @@ int main(){
     printf("%d ", ali_get(a1,i));
   printf("}\n");
   printf("O numero %d está no indice %d\n", 1, ali_find(a1, 1));
+  ali_insert_at(a1, 5, 21);
+  printf("a1 = { ");
+  for (i=0; i < t ; ++i)
+    printf("%d ", ali_get(a1,i));
+  printf("}\n");
+
   ali_destroy(a1);
   return 0;
 }
