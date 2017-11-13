c5d25bfa9e2c579766e4e99b68b1480ea548d753
Lavinia Medeiros <laviniameds@icloud.com>
2017-11-13 20:20:30 -0300

    ali_remove_from testado. Funcionando.

--- test_01.c
+++ test_01.c
@@ -4,19 +4,23 @@
 
 int main(){
   array_list_int a1 = ali_create();
-  int i,t;
+  int i;
   for (i=0; i < 8; ++i)
     ali_push_back(a1,i+1);
-  t = ali_size(a1);
-  printf("Tamanho de a1: %d\n",t);
+  printf("Tamanho de a1: %d\n",ali_size(a1));
   printf("a1 = { ");
-  for (i=0; i < t ; ++i)
+  for (i=0; i < ali_size(a1) ; ++i)
     printf("%d ", ali_get(a1,i));
   printf("}\n");
   printf("O numero %d está no indice %d\n", 1, ali_find(a1, 1));
   ali_insert_at(a1, 5, 21);
   printf("a1 = { ");
-  for (i=0; i < t ; ++i)
+  for (i=0; i < ali_size(a1) ; ++i)
+    printf("%d ", ali_get(a1,i));
+  printf("}\n");
+  ali_remove_from(a1, 5);
+  printf("a1 = { ");
+  for (i=0; i < ali_size(a1) ; ++i)
     printf("%d ", ali_get(a1,i));
   printf("}\n");
 
