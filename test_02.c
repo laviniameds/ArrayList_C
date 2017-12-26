1ec01838d87e4ba6074216f4e5a8c6a68eaccbb4
laviniameds <laviniameds@icloud.com>
2017-12-25 23:52:31 -0300

    ->teste de desempenho funcoes;

--- test_02.c
+++ test_02.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include "array_list_int.h"
 
-
 int main(){
   array_list_int a1 = ali_create();
   int i,t;
-  for (i=0; i < 8 ; ++i)
+  for (i=0; i < 1000 ; ++i)
     ali_push_back(a1,i*3+1);
   t = ali_size(a1);
   printf("Tamanho de a1: %d\n",t);
@@ -13,7 +12,22 @@ int main(){
   for (i=0; i < t ; ++i)
     printf("%d ", ali_get(a1,i));
   printf("}\n");
-  for (i=0; i < 8 ; ++i)
+
+  printf("O numero %d está no indice %d\n", 22, ali_find(a1, 22));
+
+  ali_insert_at(a1, 5, 21);
+  printf("a1 = { ");
+  for(i=0; i < ali_size(a1) ; ++i)
+    printf("%d ", ali_get(a1,i));
+  printf("}\n");
+
+  ali_remove_from(a1, 5);
+  printf("a1 = { ");
+  for (i=0; i < ali_size(a1) ; ++i)
+    printf("%d ", ali_get(a1,i));
+  printf("}\n");
+
+  for (i=0; i < ali_size(a1) ; ++i)
     ali_pop_back(a1);
   t = ali_size(a1);
   printf("Tamanho de a1: %d\n",t);
@@ -21,6 +35,8 @@ int main(){
   for (i=0; i < t ; ++i)
     printf("%d ", ali_get(a1,i));
   printf("}\n");
+
+
   ali_destroy(a1);
   return 0;
 }
